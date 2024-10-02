94c10601356d95c442d2e099cfef03e118516092
Arsenev Denis <denis.arseniev1995@gmail.com>
2024-10-02 12:24:01 +0300

    feat: sequenced realisation of task 10 with filter and fold

---
+++ src/task10/seq_filter_fold.erl
@@ -0,0 +1,15 @@
+-module(seq_filter_fold).
+
+%% API
+-export([sum_of_primes/1]).
+
+sum_of_primes(N) ->
+  List = lists:seq(2, N - 1),
+  Primes = lists:filter(fun(X) -> is_prime(X) end, List),
+  lists:foldl(fun(X, Acc) -> X + Acc end, 0, Primes).
+
+is_prime(2) -> true;
+is_prime(N) -> is_prime(N, 2).
+is_prime(N, Div) when N rem Div == 0 -> false;
+is_prime(N, Div) when Div * Div > N -> true;
+is_prime(N, Div) -> is_prime(N, Div + 1).
