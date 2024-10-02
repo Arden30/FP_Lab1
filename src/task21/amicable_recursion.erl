27175b88812eef0fa1be745011c95c9184a18a8f
Arsenev Denis <denis.arseniev1995@gmail.com>
2024-10-02 23:00:57 +0300

    feat: recursion and tail recursion form task21

---
+++ src/task21/amicable_recursion.erl
@@ -0,0 +1,19 @@
+-module(amicable_recursion).
+
+%% API
+-export([sum_of_amicable_numbers/1]).
+
+sum_of_amicable_numbers(N) -> sum_of_amicable_numbers(N, 1).
+sum_of_amicable_numbers(N, A) when N =< A -> 0;
+sum_of_amicable_numbers(N, A) ->
+  B = sum_of_proper_divisors(A),
+  D_B = sum_of_proper_divisors(B),
+  if
+    A == D_B, A =/= B -> A + sum_of_amicable_numbers(N, A + 1);
+    true -> sum_of_amicable_numbers(N, A + 1)
+  end.
+
+sum_of_proper_divisors(N) -> sum_of_proper_divisors(N, 1).
+sum_of_proper_divisors(N, Curr) when N / 2 < Curr -> 0;
+sum_of_proper_divisors(N, Curr) when N rem Curr == 0 -> Curr + sum_of_proper_divisors(N, Curr + 1);
+sum_of_proper_divisors(N, Curr) -> sum_of_proper_divisors(N, Curr + 1).
