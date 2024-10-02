7a737ed647110994a705dd735b6488c6b9776ce8
Arsenev Denis <denis.arseniev1995@gmail.com>
2024-10-03 00:10:14 +0300

    feat: filter/fold/seq solution for task21

---
+++ src/task21/amicable_seq_filter_fold.erl
@@ -0,0 +1,15 @@
+-module(amicable_seq_filter_fold).
+
+-export([sum_of_amicable_numbers/1]).
+
+sum_of_amicable_numbers(N) ->
+  Amicable_numbers = lists:filter(
+    fun(A) ->
+      B = sum_of_proper_divisors(A),
+      B =/= A andalso sum_of_proper_divisors(B) =:= A end,
+    lists:seq(1, N - 1)),
+  lists:foldl(fun(X, Acc) -> X + Acc end, 0, Amicable_numbers).
+
+sum_of_proper_divisors(N) ->
+  Divisors = lists:filter(fun(X) -> N rem X == 0 end, lists:seq(1, N div 2)),
+  lists:sum(Divisors).
