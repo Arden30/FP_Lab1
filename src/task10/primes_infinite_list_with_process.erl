51cf8094eced1d820bfb3069b3273c05aea11fcf
Arsenev Denis <denis.arseniev1995@gmail.com>
2024-10-04 14:59:19 +0300

    feat: infinite lists with processes solution for both tasks

---
+++ src/task10/primes_infinite_list_with_process.erl
@@ -0,0 +1,32 @@
+-module(primes_infinite_list_with_process).
+
+%% API
+-export([sum_of_primes/1]).
+
+sum_of_primes(N) ->
+  sum_of_primes(spawn(fun() -> generate_seq(2) end), 0, N).
+
+sum_of_primes(NextGenerator, Sum, N) ->
+  NextGenerator ! {next, self()},
+  receive
+    Num ->
+      IsPrime = is_prime(Num),
+      if
+        Num >= N -> Sum;
+        IsPrime -> sum_of_primes(NextGenerator, Sum + Num, N);
+        true -> sum_of_primes(NextGenerator, Sum, N)
+      end
+  end.
+
+generate_seq(Start) ->
+  receive
+    {next, Pid} ->
+      Pid ! Start,
+      generate_seq(Start + 1)
+  end.
+
+is_prime(2) -> true;
+is_prime(N) -> is_prime(N, 2).
+is_prime(N, Div) when N rem Div == 0 -> false;
+is_prime(N, Div) when Div * Div > N -> true;
+is_prime(N, Div) -> is_prime(N, Div + 1).
---
+++ src/task21/amicable_infinite_list_with_process.erl
@@ -0,0 +1,34 @@
+-module(amicable_infinite_list_with_process).
+
+%% API
+-export([sum_of_amicable_numbers/1]).
+
+sum_of_amicable_numbers(N) ->
+  sum_of_amicable_numbers(spawn(fun() -> generate_seq(2) end), 0, N).
+
+sum_of_amicable_numbers(NextGenerator, Sum, N) ->
+  NextGenerator ! {next, self()},
+  receive
+    Num ->
+      IsAmicable = is_amicable(Num),
+      if
+        Num >= N -> Sum;
+        IsAmicable -> sum_of_amicable_numbers(NextGenerator, Sum + Num, N);
+        true -> sum_of_amicable_numbers(NextGenerator, Sum, N)
+      end
+  end.
+
+is_amicable(A) ->
+  B = sum_of_proper_divisors(A),
+  B =/= A andalso sum_of_proper_divisors(B) =:= A.
+
+sum_of_proper_divisors(N) ->
+  Divisors = lists:filter(fun(X) -> N rem X == 0 end, lists:seq(1, N div 2)),
+  lists:sum(Divisors).
+
+generate_seq(Start) ->
+  receive
+    {next, Pid} ->
+      Pid ! Start,
+      generate_seq(Start + 1)
+  end.
