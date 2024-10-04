0c1ed5e523e88dc79a7038d8feea48e3af1fd5ef
Arsenev Denis <denis.arseniev1995@gmail.com>
2024-10-04 21:20:01 +0300

    fix: linter fix in teats (naming conventions)

--- test/task10/primes_tests.erl
+++ test/task10/primes_tests.erl
@@ -1,32 +1,32 @@
 -module(primes_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-primes_recursion_1_test() ->
+primes_recursion1_test() ->
   ?assertEqual(primes_recursion:sum_of_primes(10), 17).
 
-primes_recursion_2_test() ->
+primes_recursion2_test() ->
   ?assertEqual(primes_recursion:sum_of_primes(2000000), 142913828922).
 
-primes_tail_recursion_1_test() ->
+primes_tail_recursion1_test() ->
   ?assertEqual(primes_tail_recursion:sum_of_primes(10), 17).
 
-primes_tail_recursion_2_test() ->
+primes_tail_recursion2_test() ->
   ?assertEqual(primes_tail_recursion:sum_of_primes(2000000), 142913828922).
 
-primes_seq_filter_fold_1_test() ->
+primes_seq_filter_fold1_test() ->
   ?assertEqual(primes_seq_filter_fold:sum_of_primes(10), 17).
 
-primes_seq_filter_fold_2_test() ->
+primes_seq_filter_fold2_test() ->
   ?assertEqual(primes_seq_filter_fold:sum_of_primes(2000000), 142913828922).
 
-primes_map_1_test() ->
+primes_map1_test() ->
   ?assertEqual(primes_seq_filter_fold:sum_of_primes(10), 17).
 
-primes_map_2_test() ->
+primes_map2_test() ->
   ?assertEqual(primes_seq_filter_fold:sum_of_primes(2000000), 142913828922).
 
-primes_process_1_test() ->
+primes_process1_test() ->
   ?assertEqual(primes_infinite_list_with_process:sum_of_primes(10), 17).
 
-primes_process_2_test() ->
+primes_process2_test() ->
   ?assertEqual(primes_infinite_list_with_process:sum_of_primes(2000000), 142913828922).
--- test/task21/amicable_tests.erl
+++ test/task21/amicable_tests.erl
@@ -1,32 +1,32 @@
 -module(amicable_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-amicable_recursion_1_test() ->
+amicable_recursion1_test() ->
   ?assertEqual(amicable_recursion:sum_of_amicable_numbers(500), 504).
 
-amicable_recursion_2_test() ->
+amicable_recursion2_test() ->
   ?assertEqual(amicable_recursion:sum_of_amicable_numbers(10000), 31626).
 
-amicable_tail_recursion_1_test() ->
+amicable_tail_recursion1_test() ->
   ?assertEqual(amicable_tail_recursion:sum_of_amicable_numbers(500), 504).
 
-amicable_tail_recursion_2_test() ->
+amicable_tail_recursion2_test() ->
   ?assertEqual(amicable_tail_recursion:sum_of_amicable_numbers(10000), 31626).
 
-amicable_seq_filter_fold_1_test() ->
+amicable_seq_filter_fold1_test() ->
   ?assertEqual(amicable_seq_filter_fold:sum_of_amicable_numbers(500), 504).
 
-amicable_seq_filter_fold_2_test() ->
+amicable_seq_filter_fold2_test() ->
   ?assertEqual(amicable_seq_filter_fold:sum_of_amicable_numbers(10000), 31626).
 
-amicable_map_1_test() ->
+amicable_map1_test() ->
   ?assertEqual(amicable_map:sum_of_amicable_numbers(500), 504).
 
-amicable_map_2_test() ->
+amicable_map2_test() ->
   ?assertEqual(amicable_map:sum_of_amicable_numbers(10000), 31626).
 
-amicable_process_1_test() ->
+amicable_process1_test() ->
   ?assertEqual(amicable_infinite_list_with_process:sum_of_amicable_numbers(500), 504).
 
-amicable_process_2_test() ->
+amicable_process2_test() ->
   ?assertEqual(amicable_infinite_list_with_process:sum_of_amicable_numbers(10000), 31626).
